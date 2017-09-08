2d06ec0d723b9a37004b69955a4dd4835e212634
Rauibert <rauibertnf@gmail.com>
2017-09-08 02:54:42 +0200

    Cambios en algunas plantillas blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,13 @@
-@extends('shared.master')
-@section('title','Inicio')
+@extends('shared.master') 
+
+@section('title','Inicio') 
 
 @section('content')
 
-
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-      <div class="item active">
-        <img src="images/ibuprofeno.png" alt="Ibuprofeno" style="width:100%; height:400px">
-      </div>
-
-      <div class="item">
-        <img src="images/pastillas.jpg" alt="Pastillas" style="width:100%; height:400px">
-      </div>
-          
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
+  <div class="content">
+    <div class="title m-b-md">
+      <h1>Bienvenidos a MedApp</h1>
+    </div>  
   </div>
 
-   
 @endsection
